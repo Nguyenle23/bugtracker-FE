fb4507b7612de09eba8e68bd00a32f1fa962e1f9
Nguyenle23 <beenbin2308@gmail.com>
2022-05-06 12:41:23 +0700

    Re-organize code architecture

---
+++ src/component/Column/Column.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import './Column.scss'
+import Task from '../Task/Task.js';
+
+
+function Column() {
+    return (
+        <div className="column"> 
+          <header>
+            <h1>Board Columns</h1>
+          </header>
+
+          <ul className="task-list">
+            <Task />
+            <li className="task-item">Description</li>
+            <li className="task-item">Description</li>
+            <li className="task-item">Description</li>
+            <li className="task-item">Description</li>
+            
+          </ul>
+
+          <footer>Add another card</footer>
+        </div>
+    )
+}
+
+export default Column
---
+++ src/component/Task/Task.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Task.scss'
+
+function Task() {
+    return (
+        <li className="task-item">
+        {/* <img src="https://scontent.fsgn8-2.fna.fbcdn.net/v/t39.30808-6/280153819_6079151805464022_5185646600769582903_n.jpg?_nc_cat=1&ccb=1-6&_nc_sid=730e14&_nc_ohc=k7YYuN8bFNEAX-xxuCN&_nc_ht=scontent.fsgn8-2.fna&oh=00_AT8SOVxqkwHwcsw7Jtyo24G5CKVi-qqjUfwxtTe66uRwlQ&oe=6279912E" alt="avatar" /> */}
+        Title: B1EDEV
+      </li>
+    )
+}
+
+export default Task
